68942218f96931686684b218c54d886a42b23b73
Yash281003 <102240376+Yash281003@users.noreply.github.com>
2025-06-09 13:02:07 +0530

    change

--- Backend/src/lib/socket.ts
+++ Backend/src/lib/socket.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["ws://talkey-kappa.vercel.app"],
+    origin: ["https://talkey-kappa.vercel.app"],
   },
 });
 
--- Frontend/src/store/userAuthStore.ts
+++ Frontend/src/store/userAuthStore.ts
@@ -6,7 +6,11 @@
   import toast from "react-hot-toast";
   import * as z from 'zod'
 
-  const BASE_URL =  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+ const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://talkey-dbpu.onrender.com/api"; // ✅ use full backend domain
+
 
   export interface User {
     _id: string;

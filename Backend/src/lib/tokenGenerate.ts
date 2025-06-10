67e2a8f4a6fc5528af7d676ccf6a790088e5032c
Yash281003 <102240376+Yash281003@users.noreply.github.com>
2025-06-10 09:46:18 +0530

    cookieSetting

--- Backend/src/lib/tokenGenerate.ts
+++ Backend/src/lib/tokenGenerate.ts
@@ -2,20 +2,20 @@ import { Response } from "express";
 import jwt from "jsonwebtoken";
 import { ObjectId } from "mongoose";
 
-export const generateToken = (userId : string, res : Response) => {
-    const JWT_SECRET = process.env.JWT_SECRET;
-    if(!JWT_SECRET){
-        return;
-    }
+export const generateToken = (userId: string, res: Response) => {
+  const JWT_SECRET = process.env.JWT_SECRET;
+  if (!JWT_SECRET) {
+    return;
+  }
   const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    sameSite: "none", // ✅ allow cross-site requests
+    secure: process.env.NODE_ENV !== "development", // ✅ required when sameSite is "none"
   });
 
   return token;
